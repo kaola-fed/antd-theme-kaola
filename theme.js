04d2f67d7f4be6ec0d6dcd31b2563af9a4df7483
Cody Chan <int64ago@gmail.com>
2017-02-18 15:21:05 +0800

    feat: add theme.js

---
+++ theme.js
@@ -0,0 +1,18 @@
+module.exports = () => {
+  return {
+    'primary-color': '#51b8f1',
+    'success-color': '#58a732',
+    'warning-color': '#f0a818',
+    'error-color': '#e55c5c',
+    'line-height-base': '1.8',
+    'border-radius-base': '1px',
+    'border-radius-sm': '0',
+    'outline-width': '1px',
+    'btn-circle-size': '32px',
+    'btn-circle-size-lg': '36px',
+    'btn-circle-size-sm': '26px',
+    'input-height-base': '32px',
+    'input-height-lg': '36px',
+    'input-height-sm': '26px',
+  };
+};
